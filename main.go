1e96b109e1b79a6aa834d7a5b4c1512d9508a404
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-12-17 20:45:23 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	targetURL, err := url.Parse("http://retendo.online")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Println("Proxy server listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
